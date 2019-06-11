c8f160398e4b97ac2f03b77f174d9c01254c1dba
Bilal Majeed <bilalmajeed247@gmail.com>
2019-06-11 19:22:08 -0400

    refactor code to convert time into 24hr format

--- babysitter.py
+++ babysitter.py
@@ -1,26 +1,19 @@
 class Babysitter:
 	def __init__(self, start_time, end_time, family):
-		self.start_time = start_time
-		self.end_time = end_time
-		self.family = family
+		if start_time[-2:].upper() == "PM":
+			self.start_time = int(start_time[:-2]) + 12
+		else:
+			self.start_time = int(start_time[:-2])
 
-	def check_valid_time(self, time, am_pm):
-		if am_pm == "PM":
-			if time >= 5 and time <= 12:
-				return True
-		elif am_pm == "AM":
-			if time >= 1 and time <= 4:
-				return True
+		if end_time[-2:].upper() == "PM":
+			self.end_time = int(end_time[:-2]) + 12
+		else:
+			self.end_time = int(end_time[:-2])
 
-		return False
+		self.family = family.upper
 
 	def is_start_time_valid(self):
-		am_pm = self.start_time[-2:].upper()
-		time = int(self.start_time[:-2])
-		return self.check_valid_time(time, am_pm)
+		return self.start_time >= 17 or self.start_time <= 4
 		
-
 	def is_end_time_valid(self):
-		am_pm = self.end_time[-2:].upper()
-		time = int(self.end_time[:-2])
-		return self.check_valid_time(time, am_pm)
+		return self.end_time >= 17 or self.end_time <= 4
--- test_babysitter.py
+++ test_babysitter.py
@@ -4,20 +4,20 @@ from babysitter import Babysitter
 class TestBabysitter(unittest.TestCase):
 	def test_starts_no_earlier_than_5pm(self):
 		babysitter = Babysitter("5PM", "10PM", "a")
-		self.assertEqual(babysitter.is_start_time_valid(), True)
+		self.assertTrue(babysitter.is_start_time_valid())
 
 		babysitter = Babysitter("4PM", "10PM", "a")
-		self.assertEqual(babysitter.is_start_time_valid(), False)
+		self.assertFalse(babysitter.is_start_time_valid())
 
 		babysitter = Babysitter("1AM", "10PM", "a")
-		self.assertEqual(babysitter.is_start_time_valid(), True)
+		self.assertTrue(babysitter.is_start_time_valid())
 
 	def test_ends_no_later_than_4am(self):
 		babysitter = Babysitter("6PM", "3AM", "a")
-		self.assertEqual(babysitter.is_end_time_valid(), True)
+		self.assertTrue(babysitter.is_end_time_valid())
 
 		babysitter = Babysitter("6PM", "10PM", "a")
-		self.assertEqual(babysitter.is_end_time_valid(), True)
+		self.assertTrue(babysitter.is_end_time_valid())
 
 		babysitter = Babysitter("6PM", "6AM", "a")
-		self.assertEqual(babysitter.is_end_time_valid(), False)
+		self.assertFalse(babysitter.is_end_time_valid())
