912664b89d3786697fd6c518d38bb1e66a1b2d64
Bilal Majeed <bilalmajeed247@gmail.com>
2019-06-11 19:04:38 -0400

    test if end time is valid refactored to clean up code

--- babysitter.py
+++ babysitter.py
@@ -4,10 +4,7 @@ class Babysitter:
 		self.end_time = end_time
 		self.family = family
 
-	def is_valid_start_time(self):
-		am_pm = self.start_time[-2:].upper()
-		time = int(self.start_time[:-2])
-
+	def check_valid_time(self, time, am_pm):
 		if am_pm == "PM":
 			if time >= 5 and time <= 12:
 				return True
@@ -16,3 +13,14 @@ class Babysitter:
 				return True
 
 		return False
+
+	def is_start_time_valid(self):
+		am_pm = self.start_time[-2:].upper()
+		time = int(self.start_time[:-2])
+		return self.check_valid_time(time, am_pm)
+		
+
+	def is_end_time_valid(self):
+		am_pm = self.end_time[-2:].upper()
+		time = int(self.end_time[:-2])
+		return self.check_valid_time(time, am_pm)
--- test_babysitter.py
+++ test_babysitter.py
@@ -4,10 +4,20 @@ from babysitter import Babysitter
 class TestBabysitter(unittest.TestCase):
 	def test_starts_no_earlier_than_5pm(self):
 		babysitter = Babysitter("5PM", "10PM", "a")
-		self.assertEqual(babysitter.is_valid_start_time(), True)
+		self.assertEqual(babysitter.is_start_time_valid(), True)
 
 		babysitter = Babysitter("4PM", "10PM", "a")
-		self.assertEqual(babysitter.is_valid_start_time(), False)
+		self.assertEqual(babysitter.is_start_time_valid(), False)
 
 		babysitter = Babysitter("1AM", "10PM", "a")
-		self.assertEqual(babysitter.is_valid_start_time(), True)
+		self.assertEqual(babysitter.is_start_time_valid(), True)
+
+	def test_ends_no_later_than_4am(self):
+		babysitter = Babysitter("6PM", "3AM", "a")
+		self.assertEqual(babysitter.is_end_time_valid(), True)
+
+		babysitter = Babysitter("6PM", "10PM", "a")
+		self.assertEqual(babysitter.is_end_time_valid(), True)
+
+		babysitter = Babysitter("6PM", "6AM", "a")
+		self.assertEqual(babysitter.is_end_time_valid(), False)
