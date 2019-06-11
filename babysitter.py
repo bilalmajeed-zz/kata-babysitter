d6940903fc73f7823ae19239c66a84794970da6f
Bilal Majeed <bilalmajeed247@gmail.com>
2019-06-11 18:57:06 -0400

    refactored to enter time w/ AM and PM

--- babysitter.py
+++ babysitter.py
@@ -5,6 +5,14 @@ class Babysitter:
 		self.family = family
 
 	def is_valid_start_time(self):
-		if self.start_time < 5:
-			return False
-		return True
+		am_pm = self.start_time[-2:].upper()
+		time = int(self.start_time[:-2])
+
+		if am_pm == "PM":
+			if time >= 5 and time <= 12:
+				return True
+		elif am_pm == "AM":
+			if time >= 1 and time <= 4:
+				return True
+
+		return False
--- test_babysitter.py
+++ test_babysitter.py
@@ -3,8 +3,11 @@ from babysitter import Babysitter
 
 class TestBabysitter(unittest.TestCase):
 	def test_starts_no_earlier_than_5pm(self):
-		babysitter = Babysitter(5, 10, "a")
+		babysitter = Babysitter("5PM", "10PM", "a")
 		self.assertEqual(babysitter.is_valid_start_time(), True)
 
-		babysitter = Babysitter(4, 10, "a")
+		babysitter = Babysitter("4PM", "10PM", "a")
 		self.assertEqual(babysitter.is_valid_start_time(), False)
+
+		babysitter = Babysitter("1AM", "10PM", "a")
+		self.assertEqual(babysitter.is_valid_start_time(), True)
