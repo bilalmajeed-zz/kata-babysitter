5ff8b270fbf4ac4fd1e91623cbca585c84e301d2
Bilal Majeed <bilalmajeed247@gmail.com>
2019-06-12 13:26:41 -0400

    changed test names to be more readable

--- test_babysitter.py
+++ test_babysitter.py
@@ -2,7 +2,7 @@ import unittest
 from babysitter import Babysitter
 
 class TestBabysitter(unittest.TestCase):
-	def test_starts_no_earlier_than_5pm(self):
+	def test_that_the_start_time_no_earlier_than_5pm(self):
 		babysitter = Babysitter("5:00PM", "10:00PM", "a")
 		self.assertTrue(babysitter.is_start_time_valid())
 
@@ -15,7 +15,7 @@ class TestBabysitter(unittest.TestCase):
 		babysitter = Babysitter("4:45PM", "10:00PM", "a")
 		self.assertFalse(babysitter.is_start_time_valid())
 
-	def test_ends_no_later_than_4am(self):
+	def test_that_the_end_time_no_later_than_4am(self):
 		babysitter = Babysitter("6:00PM", "3:00AM", "a")
 		self.assertTrue(babysitter.is_end_time_valid())
 
@@ -28,7 +28,7 @@ class TestBabysitter(unittest.TestCase):
 		babysitter = Babysitter("6:00PM", "4:30AM", "a")
 		self.assertFalse(babysitter.is_end_time_valid())
 
-	def test_start_time_is_before_end_time(self):
+	def test_that_the_start_time_is_before_end_time(self):
 		babysitter = Babysitter("6:00PM", "12:00PM", "a")
 		self.assertTrue(babysitter.is_start_before_end(18, 24))
 		
@@ -44,7 +44,7 @@ class TestBabysitter(unittest.TestCase):
 		babysitter = Babysitter("1:00AM", "11:00PM", "a")
 		self.assertFalse(babysitter.is_start_before_end(1, 23))
 
-	def test_family_name_is_valid(self):
+	def test_that_a_selected_family_name_is_valid_or_not(self):
 		babysitter = Babysitter("8:00PM", "6:00PM", "a")
 		self.assertTrue(babysitter.is_family_valid())
 
@@ -57,7 +57,7 @@ class TestBabysitter(unittest.TestCase):
 		babysitter = Babysitter("8:00PM", "6:00PM", "d")
 		self.assertFalse(babysitter.is_family_valid())
 
-	def test_if_the_number_of_hrs_between_start_and_end_time_is_correct(self):
+	def test_that_the_correct_number_of_hrs_between_start_and_end_time_is_calculated(self):
 		babysitter = Babysitter("7:00PM", "2:00AM", "a")
 		self.assertEqual(babysitter.calculate_num_hrs(19, 2), 7)
 
@@ -94,7 +94,7 @@ class TestBabysitter(unittest.TestCase):
 		babysitter = Babysitter("6:00PM", "2:00AM", "c")
 		self.assertEqual(babysitter.calculate_rate(), 138)
 
-	def test_to_make_sure_only_full_hour_rates_are_calculated(self):
+	def test_that_only_full_hour_rates_are_calculated(self):
 		babysitter = Babysitter("6:30PM", "2:45AM", "c")
 		self.assertEqual(babysitter.calculate_rate(), 138)
 
@@ -104,7 +104,7 @@ class TestBabysitter(unittest.TestCase):
 		babysitter = Babysitter("6:30PM", "1:45AM", "c")
 		self.assertEqual(babysitter.calculate_rate(), 123)
 
-	def test_if_rates_are_only_be_calaculated_given_valid_inputs(self):
+	def test_that_rates_are_only_calaculated_if_inputs_are_valid(self):
 		babysitter = Babysitter("6:30PM", "1:45AM", "c")
 		self.assertEqual(babysitter.calculate_rate(), 123)
 
